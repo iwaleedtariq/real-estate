da0e1a8d2539945f9e039f1287d38b08446f310e
Waleed Tariq <dev.ratech@outlook.com>
2022-07-06 01:35:52 +0500

    add alt

--- components/SearchFilters.tsx
+++ components/SearchFilters.tsx
@@ -103,7 +103,7 @@ const SearchFilters = () => {
                                 ))}
                                 {!loading && !locationData?.length && (
                                     <Flex justifyContent='center' alignItems='center' flexDir='column' marginTop='5' marginBottom='5' >
-                                        <Image src={noresult} />
+                                        <Image alt="no result" src={noresult} />
                                         <Text fontSize='xl' marginTop='3'>
                                             Waiting to search!
                                         </Text>
